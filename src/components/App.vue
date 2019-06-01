7cc7e75f235b3b20a3047a12e2c707b2538c0426
decentraliser <jeanluc.strullu@gmail.com>
2019-06-01 22:26:55 +0800

    update travis.yaml

--- src/components/App.vue
+++ src/components/App.vue
@@ -178,7 +178,7 @@
           >
             Slack
           </v-btn>
-          <strong>NEM2 (Cow) Wallet v0.1.0alpha</strong>
+          <strong>NEM2 (Cow) Wallet v0.1.1alpha</strong>
         </v-card-text>
       </v-card>
     </v-footer>
