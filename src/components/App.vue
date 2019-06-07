e8539df2b8d3c12e59e4ecd2dc3782e2ba5f262d
Aleix <aleixmorgadas@users.noreply.github.com>
2019-06-07 12:46:07 +0200

    version 0.1.4alpha

--- src/components/App.vue
+++ src/components/App.vue
@@ -178,7 +178,7 @@
           >
             Slack
           </v-btn>
-          <strong>NEM2 (Cow) Wallet v0.1.3alpha</strong>
+          <strong>NEM2 (Cow) Wallet v0.1.4alpha</strong>
         </v-card-text>
       </v-card>
     </v-footer>
